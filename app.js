3e84231568af9a445ccccc51f9e705847339e1b3
norbertozga <norbert65389@gmail.com>
2019-10-15 13:11:38 +0200

    cors

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const pool = require('./config');
-
+const cors = require('cors');
 const checkRouter = require('./routes/check');
 const playersRouter = require('./routes/players');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 app.use('/check', checkRouter);
 app.use('/players', playersRouter);
 
